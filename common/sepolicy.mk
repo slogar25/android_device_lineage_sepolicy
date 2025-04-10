1a783d45dc89dc38e9c67030360a53866280f0fb
slogar25 <michaelslogar@gmail.com>
2025-04-10 22:50:59 +0000

    move flipendo coredomain out of vendor

--- common/sepolicy.mk
+++ common/sepolicy.mk
@@ -19,13 +19,13 @@ ifeq ($(TARGET_USES_PREBUILT_VENDOR_SEPOLICY), true)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += \
     device/lineage/sepolicy/common/dynamic \
     device/lineage/sepolicy/common/system \
-    hardware/google/pixel-sepolicy/flipendo \
+    hardware/google/pixel-sepolicy/flipendo/private \
     hardware/google/pixel-sepolicy/turbo_adapter
 else
 BOARD_VENDOR_SEPOLICY_DIRS += \
     device/lineage/sepolicy/common/dynamic \
     device/lineage/sepolicy/common/vendor \
-    hardware/google/pixel-sepolicy/flipendo \
+    hardware/google/pixel-sepolicy/flipendo/public \
     hardware/google/pixel-sepolicy/turbo_adapter
 endif
 
